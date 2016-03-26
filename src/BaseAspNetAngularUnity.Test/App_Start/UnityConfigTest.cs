b725f35081dddd70d9509137ccf80057bc3cfbf9
Wm. Barrett Simms <barrett@wbsimms.com>
2016-03-26 16:17:28 -0500

    Coverage Bump

---
+++ src/BaseAspNetAngularUnity.Test/App_Start/UnityConfigTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using BaseAspNetAngularUnity.App_Start;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAspNetAngularUnity.Test.App_Start
+{
+	[TestClass]
+	public class UnityConfigTest
+	{
+		[TestMethod]
+		public void ConstructorTest()
+		{
+			UnityConfig config = new UnityConfig();
+			Assert.IsNotNull(config);
+		}
+
+		[TestMethod]
+		public void SomeTest()
+		{
+			var container = UnityConfig.GetConfiguredContainer();
+			Assert.IsNotNull(container);
+			Assert.IsTrue(container.Registrations.Any());
+
+		}
+	}
+}
---
+++ src/BaseAspNetAngularUnity.Test/App_Start/UnityMvcActivatorTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using BaseAspNetAngularUnity.App_Start;
+using Microsoft.Practices.Unity.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAspNetAngularUnity.Test.App_Start
+{
+	[TestClass]
+	public class UnityMvcActivatorTest
+	{
+		[TestMethod]
+		public void StartStopTest()
+		{
+			Assert.IsFalse(DependencyResolver.Current is UnityDependencyResolver);
+			UnityWebActivator.Start();
+			Assert.IsTrue(DependencyResolver.Current is UnityDependencyResolver);
+		}
+	}
+}
